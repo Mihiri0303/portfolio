e9e6fa3b76cc56c43da3d32e82d029fd85d5998d
Mihiri0303 <93081@fhss.sjp.ac.lk>
2021-06-06 19:02:42 +0530

    update About

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -38,11 +38,11 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
           <div className="col-5 d-none d-lg-block align-self-center">
             {showPic && (
               <img
-                className="border border-secondary rounded-circle"
+                className="border border-secondary rounded-squars"
                 src={profilePicUrl}
                 alt="profilepicture"
-                width={imgSize}
-                height={imgSize}
+                width={375}
+                height={450}
               />
             )}
           </div>
